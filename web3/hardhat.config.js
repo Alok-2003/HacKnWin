e53a976227db3039677972d53a4550dd761663cc
Alok kumar yadav <alok953280@gmail.com>
2024-03-18 12:19:15 +0530

    done

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -2,23 +2,23 @@ require("@matterlabs/hardhat-zksync-solc");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 const PRIVATE_KEY= "af958a8a2de170ea207485572bab471f0cbdc5d5eb199a4864835c4178f4e88a"
-const RPC_URL= "https://rpc.ankr.com/polygon"
+const RPC_URL= "https://rpc.ankr.com/polygon_mumbai"
 module.exports = {
   defaultNetwork:"polygon_mumbai",
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
+  // zksolc: {
+  //   version: "1.3.9",
+  //   compilerSource: "binary",
+  //   settings: {
+  //     optimizer: {
+  //       enabled: true,
+  //     },
+  //   },
+  // },
   networks:{
     hardhat:{
       chainId:80001,
     },
-    polgon_mumbai:{
+    polygon_mumbai:{
       url: RPC_URL,
       accounts: [`0x${PRIVATE_KEY}`]
     }
